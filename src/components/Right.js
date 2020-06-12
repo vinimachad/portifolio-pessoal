03f03d094e98eb0839b07d2ff55e20f9c2c2d71d
vinimachad <viniciusgm45@gmail.com>
2020-06-12 14:22:27 -0400

    finalizado layout

--- src/components/Right.js
+++ src/components/Right.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {FaUserFriends,FaUsers,FaBook,FaAlgolia,FaHeart,FaReact,FaHtml5,FaSass,FaFigma,FaJsSquare} from 'react-icons/fa'
 import layoutPage from '../assets/layout-page.png';
+import projects from '../data'
 export default function Right(){
+
     return(
         <div id="right">
             <div className="box">
@@ -23,19 +26,96 @@ export default function Right(){
                 <hr/>
                 <div className="hidden-proj">
                     <div className="scroll-proj">
+                        {projects.map(project => (
                         <div className="box-proj">
                             <div className="name-proj">
-                                <span>( 2017 - 2018 )</span>
+                        <span>{project.name}</span>
                             </div>
                             <div className="content-proj">
                                 <p>
-
+                                    {project.content}
                                 </p>
+                                <a target="_blank" href={project.linkGit}>Acessar repositiorio no GitHub</a>
                             </div>
                         </div>
+                        ))}
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="skills">
+                <strong>Habilidades</strong>
+                <hr/>
+                <div className="division">
+                        <div className="pessoal-skill">
+                            <ul>
+                                <li>
+                                    <div className="rounded-icons">
+                                        <FaUserFriends/>
+                                    </div>
+                                    <span>Amigavél</span>
+                                </li>
+                                <li>    
+                                    <div className="rounded-icons">
+                                        <FaUsers/>
+                                    </div>           
+                                    <span>Trabalho<br/> em Grupo</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">  
+                                        <FaBook/>
+                                    </div>
+                                    <span>Constante Aprendizado</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">    
+                                        <FaAlgolia/>
+                                    </div>
+                                    <span>Projetos nos Prazos</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">
+                                        <FaHeart/>
+                                    </div>
+                                    <span>Apaixonado por tecnolgia</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="tools-skills">
+                            <ul>
+                                <li>
+                                    <div className="rounded-icons">
+                                        <FaHtml5/>
+                                    </div>
+                                    <span>HTML5</span>
+                                </li>
+                                <li>    
+                                    <div className="rounded-icons">
+                                        <FaReact/>
+                                    </div>           
+                                    <span>React JS</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">  
+                                        <FaSass/>
+                                    </div>
+                                    <span>Sass</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">    
+                                        <FaJsSquare/>
+                                    </div>
+                                    <span>JavaScript</span>
+                                </li>
+                                <li>
+                                    <div className="rounded-icons">
+                                        <FaFigma/>
+                                    </div>
+                                    <span>Figma</span>
+                                </li>
+                            </ul>
+                        </div> 
+                    </div>
+                </div>
+            </div>
     );
 }
