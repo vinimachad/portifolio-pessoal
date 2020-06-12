432df0ecb327ffe016e7a3868c84631139106d7b
vinimachad <viniciusgm45@gmail.com>
2020-06-12 01:43:08 -0400

    parte de contact adicionada

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaMapMarkerAlt} from 'react-icons/fa';
+import {FaMapMarkerAlt,FaPhoneAlt,FaPhoneSquareAlt,FaInstagramSquare,FaFacebookSquare,FaGithubSquare,FaMailBulk} from 'react-icons/fa';
 import ft from '../assets/IMG_20191221_163648_506.jpg'
 export default function Left() {
     return(
@@ -41,7 +41,20 @@ export default function Left() {
                         </ul>
                     </div>
                 </div>
-                <div className="contact"></div>
+                <div className="contact">
+                    <FaPhoneAlt/>
+                    <div className="about-contact">
+                        <strong>Contato</strong>
+                        <hr/>
+                        <ul>
+                            <li><FaPhoneSquareAlt/> (67) 9 9229-3645</li>
+                            <li><FaInstagramSquare/><a target="_blank" href="https://www.instagram.com/vinimachad/">vinimachad</a></li>
+                            <li><FaFacebookSquare/><a target="_blank" href="https://www.facebook.com/vinicius.galhardomachado">Vinicius Galhardo</a></li>
+                            <li><FaGithubSquare/> <a target="_blank" href="https://github.com/vinimachad">Vincius Galhardo Machado</a></li>
+                            <li><FaMailBulk/> <a target="_blank" href="">viniciusgm45@gmail.com</a></li>
+                        </ul>
+                    </div>
+                </div>
                 <div className="social"></div>
             </div>
         </div>
